6919a171f10b84c79983f5548af77560fb48b4f8
Bianca Jayaraman <133613936+biancajayy@users.noreply.github.com>
2023-11-03 23:56:30 -0400

    Add files via upload

---
+++ LinkedList/LinkedQueue.java
@@ -0,0 +1,127 @@
+import java.util.NoSuchElementException;
+
+/**
+ * Your implementation of a LinkedQueue. It should NOT be circular.
+ *
+ * @author Bianca Jayaraman
+ * @version 1.0
+ * @userid bjayaraman9
+ * @GTID 903754012
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class LinkedQueue<T> {
+
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private LinkedNode<T> head;
+    private LinkedNode<T> tail;
+    private int size;
+
+    /*
+     * Do not add a constructor.
+     */
+
+    /**
+     * Adds the data to the back of the queue.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the back of the queue
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void enqueue(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("This method does not allow "
+                    + "null data to be added to the data structure.");
+        }
+        LinkedNode<T> newNode = new LinkedNode<>(data);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = tail.getNext();
+        }
+        size++;
+    }
+
+    /**
+     * Removes and returns the data from the front of the queue.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T dequeue() {
+        if (size == 0) {
+            throw new NoSuchElementException("The data structure is "
+                    + "empty, so no elements can be removed.");
+        }
+        LinkedNode<T> oldHead = head;
+        head = head.getNext();
+        oldHead.setNext(null);
+        size--;
+        return oldHead.getData();
+    }
+
+
+    /**
+     * Returns the data from the front of the queue without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the front of the queue
+     * @throws java.util.NoSuchElementException if the queue is empty
+     */
+    public T peek() {
+        if (size == 0) {
+            throw new NoSuchElementException("The data structure is "
+                    + "empty, so no elements can be removed.");
+        }
+        return head.getData();
+    }
+
+    /**
+     * Returns the head node of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the head of the queue
+     */
+    public LinkedNode<T> getHead() {
+        // DO NOT MODIFY THIS METHOD!
+        return head;
+    }
+
+    /**
+     * Returns the tail node of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the tail of the queue
+     */
+    public LinkedNode<T> getTail() {
+        // DO NOT MODIFY THIS METHOD!
+        return tail;
+    }
+
+    /**
+     * Returns the size of the queue.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the queue
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
